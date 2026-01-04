32c46d2f76f9eac2d5ea1b565fd8cec3a111f4ad
Rushikeshegderushikesh <hegderusikesh@gmail.com>
2026-01-04 19:52:23 +0530

    second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
-import Paste from './components/paste'
+import Paste from './components/Paste'
 import Viewpaste from './components/Viewpaste'
 
 function App() {
